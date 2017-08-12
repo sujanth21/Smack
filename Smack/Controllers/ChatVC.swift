c38d0ba31d849abd566ffffbac18c0b8eb0d4b3c
Sujanth <sujanth1987@gmail.com>
2017-08-12 12:26:10 +1000

    View controllers added

---
+++ Smack/Controllers/ChatVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Sujanth Sebamalaithasan on 12/8/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
