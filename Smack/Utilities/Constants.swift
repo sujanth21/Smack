ddf6bbf9d9b2ad71969e2fb089c4b1f3fd8375aa
Sujanth <sujanth1987@gmail.com>
2017-08-14 21:23:36 +1000

    Register user func added

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = "https://smackychatchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
