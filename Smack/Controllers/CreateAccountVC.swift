2cd7f4d897ad31f00014ec6a2f7eb77e8bc525fd
Sujanth <sujanth1987@gmail.com>
2017-08-17 08:10:39 +1000

    Created login function

--- Smack/Controllers/CreateAccountVC.swift
+++ Smack/Controllers/CreateAccountVC.swift
@@ -31,7 +31,13 @@ class CreateAccountVC: UIViewController {
         
         AuthService.instance.registerUser(email: email, password: password) { (success) in
             if success {
-                print("Registered user!")
+                
+                AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
+                    
+                    if success {
+                        print("Logged in user!", AuthService.instance.authToken)
+                    }
+                })
             }
         }
     }
