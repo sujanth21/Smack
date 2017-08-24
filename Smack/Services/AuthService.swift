21b1671d184bb87b9600238e73376330a63db30d
Sujanth <sujanth1987@gmail.com>
2017-08-24 22:29:03 +1000

    SwiftyJSON refactored

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -75,17 +76,11 @@ class AuthService {
             
             if response.result.error == nil {
                 
-                if let json = response.result.value as? Dictionary<String, Any> {
-                    
-                    if let email = json["user"] as? String {
-                        self.userEmail = email
-                    }
-                    
-                    if let token = json["token"] as? String {
-                        self.authToken = token
-                    }
-                }
-                
+                guard let data = response.data else { return }
+                let json = JSON(data: data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
+            
                 self.isLoggedIn = true
                 completion(true)
             } else {
